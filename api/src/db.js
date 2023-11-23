77723d25301679bdc20c89cd33ca8fbbd27959c0
Santiago Bosca Chillida <borkoloco@MacBook-Air-de-Santiago.local>
2023-11-23 01:06:56 -0300

    agregada relacion menu user

--- api/src/db.js
+++ api/src/db.js
@@ -60,6 +60,9 @@ User.belongsTo(Role, {
   as: "roleUser",
 });
 
+User.belongsToMany(Menu, { through: "orden" });
+Menu.belongsToMany(User, { through: "orden" });
+
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
