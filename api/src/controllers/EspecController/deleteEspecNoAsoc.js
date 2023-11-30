9c7b7784c308d7dd9cbc9ba687c16cfca92fe822
Santiago Bosca Chillida <borkoloco@MacBook-Air-de-Santiago.local>
2023-11-30 15:32:47 -0300

    arreglda barra y otros

--- api/src/controllers/EspecController/deleteEspecNoAsoc.js
+++ api/src/controllers/EspecController/deleteEspecNoAsoc.js
@@ -1,31 +1,28 @@
-const { Especialidad, User } = require("../../db");
+const { Especialidad, Menu } = require("../../db");
 
 const deleteEspec2 = async (req, res) => {
-  const { idEspecialidad } = req.params;
+  const { id } = req.params;
 
-  if (!idEspecialidad) {
-    return res.status(400).send("Se requiere idEspecialidad en los parámetros");
+  if (!id) {
+    return res.status(400).send("Se requiere id para la Especialidad");
   }
 
   try {
-    const especialidad = await Especialidad.findByPk(idEspecialidad);
+    const especialidad = await Especialidad.findByPk(id);
 
     if (!especialidad) {
       return res.status(404).send("Especialidad no encontrada");
     }
 
-    const usersAsociados = await User.findAll({
-      include: {
-        model: Especialidad,
-        where: { id: especialidad.id },
-      },
+    const menuAsociado = await Menu.findOne({
+      where: { idEspecialidad: id },
     });
 
-    if (usersAsociados.length > 0) {
+    if (menuAsociado) {
       return res
         .status(403)
         .send(
-          "No puedes eliminar esta Especialidad porque está asociada a un Usuario"
+          "No puedes eliminar esta Especialidad porque está asociada a un Menu"
         );
     }
 
