e62e514196906566c1b8b97af3ff47f7f4480de1
Santiago Bosca Chillida <borkoloco@MacBook-Air-de-Santiago.local>
2023-12-12 17:39:12 -0300

    sacados los admin

--- api/src/controllers/UserController/getUsersBanned.js
+++ api/src/controllers/UserController/getUsersBanned.js
@@ -1,13 +1,24 @@
-const { User } = require("../../db");
+const { User, Role } = require("../../db");
 
 const getUsersBanned = async (req, res) => {
   try {
-    const users = await User.findAll({
+    const usersWithoutAdminRole = await User.findAll({
+      include: [
+        {
+          model: Role,
+          where: {
+            nameRole: {
+              [Sequelize.Op.notILike]: "%admin%",
+            },
+          },
+          required: false,
+        },
+      ],
       order: [["email", "ASC"]],
       attributes: ["nameUser", "isBanned", "email"],
     });
 
-    res.status(200).json(users);
+    res.status(200).json(usersWithoutAdminRole);
   } catch (error) {
     res
       .status(500)
@@ -16,3 +27,22 @@ const getUsersBanned = async (req, res) => {
 };
 
 module.exports = getUsersBanned;
+
+// const { User } = require("../../db");
+
+// const getUsersBanned = async (req, res) => {
+//   try {
+//     const users = await User.findAll({
+//       order: [["email", "ASC"]],
+//       attributes: ["nameUser", "isBanned", "email"],
+//     });
+
+//     res.status(200).json(users);
+//   } catch (error) {
+//     res
+//       .status(500)
+//       .json({ error: "Hubo un problema al obtener los usuarios." });
+//   }
+// };
+
+// module.exports = getUsersBanned;
