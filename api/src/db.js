72cc3c6610d7c1078344379e2d9f832ccc201bce
Santiago Bosca Chillida <borkoloco@MacBook-Air-de-Santiago.local>
2024-02-28 19:35:46 -0300

    coso

--- api/src/db.js
+++ api/src/db.js
@@ -8,8 +8,8 @@ const sequelize = new Sequelize(
   // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/menu`,
   `${DB_DEPLOY}`,
   {
-    // logging: false, // set to console.log to see the raw SQL queries
-    // native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     dialectOptions: {
       ssl: {
         require: true,
